a2bc9c413d1b2eab3ce48c3eb7d4104e770029a8
dtabuena <52722000+dtabuena@users.noreply.github.com>
2026-02-01 23:32:16 -0800

    Update __init__.py

--- patchclamp_analysis/__init__.py
+++ patchclamp_analysis/__init__.py
@@ -4,3 +4,12 @@ from . import (
     # IV_analyzer,
     # uncomment as modules are added
 )
+
+
+"""
+import numpy as np
+import scipy as sci
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import os
+"""
