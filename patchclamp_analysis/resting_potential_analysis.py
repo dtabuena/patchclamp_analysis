cefc0ba0cc463565f980800f64bc41c891642a13
dtabuena <52722000+dtabuena@users.noreply.github.com>
2026-02-03 22:28:26 -0800

    Create resting_potential_analysis.py

---
+++ patchclamp_analysis/resting_potential_analysis.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+def rmp_analyzer(abf,to_plot=False,figopt={'type':'jpg'}):
+    results = {'Rmp_mV': np.nan}
+    if 'mV' not in abf.adcUnits[0]:
+        return {'Rmp_mV': "VCLAMP_DO_PSCs"}
+    abf.setSweep(0,0)
+    command = abf.sweepC[:abf.sampleRate*3]
+    vm = abf.sweepY[:abf.sampleRate*3]
+    abf.setSweep(0,1)
+    command_ch2 = abf.sweepY[:abf.sampleRate*3]
+    if to_plot:
+        fig, ax = plt.subplots(1,2,figsize=(3,1.5))
+        ax[0].hist(command,20)
+        ax[0].hist(command_ch2,20)
+        ax[0].set_xlabel('Command Current (pA)')
+        ax[1].hist(vm,20)
+        ax[1].set_xlabel('Membrane Pot (mv)')
+        os.makedirs('Saved_Figs/Resting_VM/', exist_ok=True)
+        fig.savefig( 'Saved_Figs/Resting_VM/Resting_VM_' + abf.abfID +'.'+figopt['type'])
+        plt.tight_layout()
+        plt.show()
+    mean_rmp = np.mean(vm)
+    mean_command = np.mean(command_ch2)
+    if abs(mean_command)<15:
+        results = {'Rmp_mV': mean_rmp,'command':mean_command}
+    return results
